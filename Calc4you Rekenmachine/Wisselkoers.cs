194bfd3edb23ecf1539bedd6b4e958197d56cf7d
Kortom-Tom <0322467@student.rocvantwente.nl>
2019-11-20 16:29:32 +0100

    Comments maken in de API call

--- Calc4you Rekenmachine/Wisselkoers.cs
+++ Calc4you Rekenmachine/Wisselkoers.cs
@@ -18,11 +18,17 @@ namespace Calc4you_Rekenmachine
             {
                 client.BaseAddress = new Uri("https://api.exchangeratesapi.io/latest");
                 HttpResponseMessage response = client.GetAsync("?symbols=USD").Result;
+                // "?symbols=USD" is de achter geplakt
                 response.EnsureSuccessStatusCode();
+                // API CALL en doe dingen
                 string result = response.Content.ReadAsStringAsync().Result;
+                // string naar result zetten
                 string USD = result.Substring(16, 6);
+                // van het begin 16 chars eraf en vanaf daar 6 chars nemen
                 string USD1 = USD.Replace(".", ",");
+                // verander de punt met een komma
                 decimal USD2 = Convert.ToDecimal(USD1);
+                // convert naar decimal
 
                 return _bedragInEuro * USD2;
                 // API CALL
