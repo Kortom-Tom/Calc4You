2c1d87b673c1951bd7e76f4ac3b08bb1ea64ac52
Kortom-Tom <0322467@student.rocvantwente.nl>
2019-11-20 10:02:05 +0100

    Add WIsselKoers

---
+++ wisselkoers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public static class Wisselkoers
+    {
+        public static double USD2 { get; private set; }
+
+        public static decimal EuroDollarKoers(decimal _bedragInEuro)
+        {
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+            {
+                client.BaseAddress = new Uri("https://api.exchangeratesapi.io/latest");
+                HttpResponseMessage response = client.GetAsync("?symbols=USD").Result;
+                response.EnsureSuccessStatusCode();
+                string result = response.Content.ReadAsStringAsync().Result;
+                string USD = result.Substring(16, 6);
+                string USD1 = USD.Replace(".", ",");
+                decimal USD2 = Convert.ToDecimal(USD1);
+
+                return _bedragInEuro * USD2;
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
