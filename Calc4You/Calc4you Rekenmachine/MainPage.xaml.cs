f68db31c931b63bdbefd69ef70b9ee1b11a4de77
ankidooom <[0309539@student.rocvantwente.nl]>
2019-11-21 14:31:15 +0100

    Knoppen min plus keer etc toegevoegd maar delen doet het nog niet zo goed.

--- Calc4You/Calc4you Rekenmachine/MainPage.xaml.cs
+++ Calc4You/Calc4you Rekenmachine/MainPage.xaml.cs
@@ -22,6 +22,9 @@ namespace Calc4you_Rekenmachine
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        bool Knopplus;
+        bool Knopmin;
+        bool Knopkeer;
         public MainPage()
         {
             this.InitializeComponent();
@@ -29,56 +32,60 @@ namespace Calc4you_Rekenmachine
         #region Nummer van rekenmachine.
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            TextBox.Text = TextBox.Text + "1";
+            TextBox.Text += 1;
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-            TextBox.Text = TextBox.Text + "2";
+            TextBox.Text += 2;
         }
         private void button3_Click(object sender, RoutedEventArgs e)
         {
-            TextBox.Text = TextBox.Text + "3";
+            TextBox.Text += 3;
         }
 
         private void button4_Click(object sender, RoutedEventArgs e)
         {
-            TextBox.Text = TextBox.Text + "4";
+            TextBox.Text += 4;
         }
 
         private void button5_Click(object sender, RoutedEventArgs e)
         {
-            TextBox.Text = TextBox.Text + "5";
+            TextBox.Text += 5;
         }
 
         private void button6_Click(object sender, RoutedEventArgs e)
         {
-            TextBox.Text = TextBox.Text + "6";
+            TextBox.Text += 6;
         }
 
         private void button7_Click(object sender, RoutedEventArgs e)
         {
-            TextBox.Text = TextBox.Text + "7";
+            TextBox.Text += 7;
         }
 
         private void button8_Click(object sender, RoutedEventArgs e)
         {
-            TextBox.Text = TextBox.Text + "8";
+            TextBox.Text += 8;
         }
 
         private void button9_Click(object sender, RoutedEventArgs e)
         {
-            TextBox.Text = TextBox.Text + "9";
+            TextBox.Text += 9;
         }
         private void button0_Click(object sender, RoutedEventArgs e)
         {
-            TextBox.Text = TextBox.Text + "0";
+            TextBox.Text += 0;
         }
-        #region punt en komma onder de cijfer van rekenmachine.
+        #region  komma en procenten onder de cijfer van rekenmachine.
         private void buttonPunt_Click(object sender, RoutedEventArgs e)
         {
             TextBox.Text = TextBox.Text + ",";
             buttonPunt.IsEnabled = false;
+        }
+        private void buttonProcent_Click(object sender, RoutedEventArgs e)
+        {
+
         }
         #endregion
         #endregion
@@ -86,12 +93,95 @@ namespace Calc4you_Rekenmachine
         private void name_Click(object sender, RoutedEventArgs e)
         {
             TextBox.Text = "";
+            Textbox2.Text = "";
             buttonPunt.IsEnabled = true;
         }
 
-        private void buttonProcent_Click(object sender, RoutedEventArgs e)
+        #region buttons van plus win keer delen en = knop en andere.
+        public void buttonPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+            Textbox2.Text = TextBox.Text + "";
+            TextBox.Text = "";
+            Knopplus = true;
+        }
+
+        public void buttonMin_Click(object sender, RoutedEventArgs e)
+        {
+            Textbox2.Text = TextBox.Text + "";
+            TextBox.Text = "";
+            Knopmin = true;
+        }
+
+        public void buttonKeer_Click(object sender, RoutedEventArgs e)
+        {
+            Textbox2.Text = TextBox.Text + "";
+            TextBox.Text = "";
+            Knopkeer = true;
+        }
+
+        public void buttonDelen_Click(object sender, RoutedEventArgs e)
+        {
+            Textbox2.Text = TextBox.Text + "";
+            TextBox.Text = "";
+        }
+
+        public void buttonIs(object sender, RoutedEventArgs e)
+        {
+            int getal2 = Convert.ToInt32(Textbox2.Text);
+            int getal1 = Convert.ToInt32(TextBox.Text);
+            if (Knopplus == true)
+            {
+                int antwoord = (getal1 + getal2);
+                TextBox.Text = Convert.ToString(antwoord);
+                getal1 = 0;
+                getal2 = 0;
+                Knopplus = false;
+            }
+            else if (Knopmin == true)
+            {
+                int antwoord = (getal1 - getal2);
+                TextBox.Text = Convert.ToString(antwoord);
+                getal1 = 0;
+                getal2 = 0;
+                Knopmin = false;
+            }
+            else if (Knopkeer == true)
+            {
+                int antwoord = (getal1 * getal2);
+                TextBox.Text = Convert.ToString(antwoord);
+                getal1 = 0;
+                getal2 = 0;
+                Knopkeer = false;
+            }
+            else
+            {
+                int antwoord = (getal1 / getal2);
+                TextBox.Text = Convert.ToString(antwoord);
+                getal1 = 0;
+                getal2 = 0;
+
+            }
+        }
+        #endregion
+
+        #region Textboxes aka de output.
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
+
+
+        private void Textbox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        #endregion
+        #region functions
+        /// public int Berekenen(int getal1, int getal2)
+        ///{
+        ///    getal1 = Convert.ToInt32(TextBox.Text);        
+        ///}
+        #endregion
     }
 }
